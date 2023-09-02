ec34dd3c521bb2f148252aee20bd18b54ec01b37
WebRizen <webrizen@gmail.com>
2023-09-02 12:41:23 +0530

    it's done About us

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,26 +2,26 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-export default function page() {
+export default function Page() {
   return (
     <>
-      <section className="py-12 min-h-screen flex flex-col justify-center items-center text-center">
+      <section className="py-12 min-h-screen flex flex-col justify-center items-center text-center ">
         <div className="container mx-auto px-4">
           <div className="max-w-3xl mx-auto text-center">
             <h2 className="text-3xl font-semibold mb-4">About Us</h2>
-            <p className="text-lg ">
+            <p className="text-lg">
               LockNotes is a secure and user-friendly web application designed
               to help you protect and share your sensitive information. Our
               mission is to provide a safe and convenient platform for creating
               and sending self-destructing notes with end-to-end encryption.
             </p>
-            <p className="text-lg  mt-4">
+            <p className="text-lg mt-4">
               Whether you need to share passwords, personal messages, or
               confidential information, LockNotes has you covered. We prioritize
               your privacy and security, ensuring that only you and your
               intended recipient can access the notes you send.
             </p>
-            <p className="text-lg  mt-4">
+            <p className="text-lg mt-4">
               Thank you for choosing LockNotes to safeguard your digital
               communications. If you have any questions or feedback, please
               don't hesitate to{" "}
@@ -33,12 +33,12 @@ export default function page() {
           </div>
         </div>
       </section>
-      <section className="py-12 min-h-screen flex flex-col justify-center items-center text-center">
+      <section className="py-12 min-h-screen flex flex-col justify-center items-center text-center ">
         <div className="container mx-auto px-4">
           <div className="max-w-3xl mx-auto text-center">
             <h2 className="text-3xl font-semibold mb-4">Our Founders</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <div className="rounded-lg shadow-md p-6 border dark:border-gray-900">
+              <div className="rounded-lg shadow-2xl p-6 flex flex-col gap-2">
                 <Image
                   src="https://cdn.truelancer.com/user-picture/2291453-64bde53e0a49b.jpg"
                   alt="Arshahdul Ahmed"
@@ -47,7 +47,8 @@ export default function page() {
                   height={200}
                 />
                 <h3 className="text-xl font-semibold">Arshahdul Ahmed</h3>
-                <p className="dark:text-gray-400">
+                <span className="text-gray-600">CTO & Founder</span>
+                <p>
                   Meet Arshahdul Ahmed, a 17-year-old web wizard with a passion
                   for technology. As an experienced Full Stack Developer,
                   Arshahdul has navigated diverse landscapes, creating seamless
@@ -63,17 +64,18 @@ export default function page() {
                   reality, one innovation at a time!
                 </p>
               </div>
-              <div className="rounded-lg shadow-md p-6 border dark:border-gray-900">
+              <div className="rounded-lg shadow-2xl p-6 flex flex-col gap-2">
                 <Image
                   src="https://cdn.truelancer.com/user-picture/2281329-64ae8cd501d65.jpg"
-                  alt="Karan Singh"
+                  alt="Harshit Jaiswal"
                   className="w-24 h-24 mx-auto rounded-full mb-4"
                   width={200}
                   height={200}
                 />
-                <h3 className="text-xl font-semibold">Karan Singh</h3>
-                <p className="dark:text-gray-400">
-                  Introducing Karan Singh, a 23-year-old Website Developer and
+                <h3 className="text-xl font-semibold">Harshit Jaiswal</h3>
+                <span className="text-gray-600">CEO & Co-Founder</span>
+                <p>
+                  Introducing Harshit Jaiswal, a 21-year-old Website Developer and
                   App Developer. His passion lies in WordPress development,
                   aiming to understand clients' requirements precisely and
                   deliver extraordinary results. Karan specializes in designing
