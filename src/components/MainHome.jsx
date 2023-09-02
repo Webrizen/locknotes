01d1489ae46db2b8524e6752c6ba724994cb9ea1
WebRizen <webrizen@gmail.com>
2023-09-02 11:53:47 +0530

    yoo

--- src/components/MainHome.jsx
+++ src/components/MainHome.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState, useRef } from "react";
 import {
   Button,
   Dialog,
@@ -10,8 +10,11 @@ import {
   Typography,
   Textarea,
   Chip,
+  IconButton,
 } from "@material-tailwind/react";
 import Link from "next/link";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import Swal from 'sweetalert2';
 
 function Icon() {
   return (
@@ -33,10 +36,34 @@ function Icon() {
 }
 
 export default function MainHome() {
+  const firestore = getFirestore();
   const [open, setOpen] = React.useState(false);
+  const [noteData, setNoteData] = useState("");
+  const [generatedLink, setGeneratedLink] = useState("");
+  const linkInputRef = useRef(null);
   const handleOpen = () => setOpen((cur) => !cur);
+   // Function to show a SweetAlert2 success notification
+   const showSuccessNotification = (message) => {
+    Swal.fire({
+      icon: "success",
+      title: "Success",
+      text: message,
+    });
+  };
+
+  // Function to show a SweetAlert2 error notification
+  const showErrorNotification = (message) => {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: message,
+    });
+  };
+
   return (
-    <section className={`hero min-h-screen flex flex-col items-center justify-center text-center relative`}>
+    <section
+      className={`hero min-h-screen flex flex-col items-center justify-center text-center relative`}
+    >
       <div className="grid grid-cols-4 absolute inset-0 -z-10 w-10/12 h-4/5 m-auto">
         {[...Array(16)].map((_, index) => (
           <div
@@ -77,7 +104,7 @@ export default function MainHome() {
         size="xs"
         open={open}
         handler={handleOpen}
-        className="bg-transparent shadow-none"
+        className="bg-transparent shadow-none z-10"
       >
         <Card className="mx-auto w-full max-w-[24rem]">
           <CardHeader
@@ -90,14 +117,71 @@ export default function MainHome() {
             </Typography>
           </CardHeader>
           <CardBody className="flex flex-col gap-2">
-            <Textarea label="Type Your Secret Notes" size="lg" />
+            <Textarea
+              label="Type Your Secret Notes"
+              size="lg"
+              onChange={(e) => setNoteData(e.target.value)}
+            />
           </CardBody>
           <CardFooter className="pt-0">
-            <Button variant="gradient" onClick={handleOpen} fullWidth>
+            <Button
+              variant="gradient"
+              onClick={async () => {
+                try {
+                  const docRef = await addDoc(collection(firestore, "notes"), {
+                    message: noteData,
+                  });
+                  const noteId = docRef.id;
+                  setGeneratedLink(
+                    `https://locknotes.vercel.app/hidden/${noteId}`
+                  );
+                  showSuccessNotification("Note created successfully!");
+                } catch (error) {
+                  console.error("Error adding document: ", error);
+                  showErrorNotification("Error creating note.");
+                }
+              }}
+              fullWidth
+            >
               Create
             </Button>
             <Typography variant="small" className="mt-6 flex justify-center">
-              Your Link Will Be Genrated After Note Creation*
+              {generatedLink && (
+                <>
+                  <input
+                    type="text"
+                    value={generatedLink}
+                    readOnly
+                    className="w-full p-4 outline-none border-gray-100"
+                    ref={linkInputRef}
+                  />
+                  <IconButton
+                  onClick={() => {
+                    if (linkInputRef.current) {
+                      linkInputRef.current.select();
+                      document.execCommand("copy");
+                      linkInputRef.current.setSelectionRange(0, 0);
+                      showSuccessNotification("Link copied to clipboard!");
+                    }
+                  }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-4 h-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+                      />
+                    </svg>
+                  </IconButton>
+                </>
+              )}
             </Typography>
           </CardFooter>
         </Card>
